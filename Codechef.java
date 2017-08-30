71c4a930edaa6d90035126a20a762306c4515b35
saiganeshrajakommuri <saiganeshrajakommuri@gmail.com>
2017-08-30 23:15:51 +0530

    First

---
+++ Codechef.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+
+
+    DataInputStream ds=new DataInputStream(System.in);
+  String   s=ds.readLine();
+String s1=s.getclass().getSimpleName();
+     System.out.println(s1);
+     
+
+
+	}
+}
